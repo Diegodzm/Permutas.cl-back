00d9b85b56008e26f248d26c74551e223d9c926e
DiegoDiaz <diegodiazmed93@gmail.com>
2024-04-24 20:39:47 -0400

    1

---
+++ app.py
@@ -0,0 +1,96 @@
+import os
+from flask import Flask,request,jsonify
+from models import db, User
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import (JWTManager,create_access_token,get_jwt_identity, jwt_required)
+
+BASEDIR= os.path.abspath(os.path.dirname(__file__))
+
+app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///" +os.path.join(BASEDIR,"blog.db")
+app.config['JWT_SECRET_KEY']= 'secret-code-01'
+db.init_app(app)
+Migrate(app,db)
+CORS(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+
+@app.route('/')
+def home():
+    return 'hello'
+@app.route('/user/register', methods=['POST'])
+def user_register():
+     email= request.json.get('email')
+     if email is not None:
+        user = User.query.filter_by(email=email).first()
+        if user is not None:
+            return jsonify({
+                "msg":"Este usuario ya existe"
+                }),400
+        else:
+            user=User()
+            password= request.json.get('password')
+            password_hash= bcrypt.generate_password_hash(password)
+
+            user.password =password_hash
+            user.email= email
+            
+
+            db.session.add(user)
+            db.session.commit()
+
+            return jsonify({
+                "msg":"user created"
+            }), 200
+
+     else:
+        return jsonify({
+            "msg": "email is required"
+        }),400
+
+@app.route('/user/login',methods=['POST'])
+def user_loguin():
+    email = request.json.get('email')
+    password = request.json.get('password')
+    if email is not None:
+        user= User.query.filter_by(email=email).first()
+        if user is not None:
+            is_valid= bcrypt.check_password_hash(user.password,password)
+            if is_valid:
+                access_token= create_access_token(identity=email)
+                return jsonify({
+                    "user":user.serialize(),
+                    "access_token": access_token,
+                    "msg":"user login"
+                }),200
+            else:
+                return jsonify({
+                    "msg":"credentials are not valid"
+                }),400
+        else: 
+            return jsonify({
+                "msg":"user not found"
+            }),400
+
+    else:
+        return jsonify({
+            "msg": "email doesnt exist"
+        }),400
+
+@app.route('/users', methods=['GET'])
+@jwt_required()
+def get_users():
+    users= User.query.all()
+    users=   list(map(lambda user:user.serialize(),users))
+
+    return jsonify(users),200
+
+
+        
+
+
+if __name__  == '__main__':
+    app.run(host='localhost',port=5000)
---
+++ models.py
@@ -0,0 +1,87 @@
+from flask_sqlalchemy import SQLAlchemy;
+
+db= SQLAlchemy()
+
+class Comment(db.Model):
+    __tablename__= 'comment'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id= db.Column(db.Integer, db.ForeignKey('user.id'))
+    body = db.Column(db.String(200), nullable=False)
+    
+class User(db.Model):
+    __tablename__= 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    firstname= db.Column(db.String(80), unique=False, nullable=False)
+    lastname= db.Column(db.String(80), unique=False, nullable=False)
+    username= db.Column(db.String(80), unique=False, nullable=False)
+    address= db.Column(db.String(80), unique=False, nullable=False)
+    user_calification = db.Column(db.Integer, unique=False, nullable=False)
+    comments= db.relationship('Comment', backref='user')
+    wishes = db.relationship('Wishlist', backref='user')
+    products= db.relationship('Product', backref='user')
+    
+    def __repr__(self):
+        return f'<User {self.email}>'
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+
+class Wishlist(db.Model):
+    __tablename__ = 'wishlist'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    publication_id = db.Column(db.Integer, db.ForeignKey('publication.id'))
+
+class Publication(db.Model):
+    __tablename__= 'publication'
+    id = db.Column(db.Integer, primary_key=True)
+    value= db.Column(db.Integer, nullable=False)
+    product_id = db.Column(db.Integer, nullable=False)
+    product_description = db.Column(db.String(400), nullable=False)
+    state = db.Column(db.Boolean, nullable=False)
+    offer_id = db.Column(db.Integer, db.ForeignKey('offer.id'))
+    wishes = db.relationship('Wishlist', backref='publication') 
+
+
+
+
+class Category(db.Model):
+    __tablename__= 'category'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    products= db.relationship('Product', backref='category')
+
+class Product(db.Model):
+    __tablename__= 'product'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    photo = db.Column(db.String(200), nullable=False)
+    product_info = db.Column(db.String(400), nullable=False)
+    brand = db.Column(db.String(200), nullable=False)
+    state = db.Column(db.Boolean, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    category_id= db.Column(db.Integer, db.ForeignKey('category.id'))
+    offers= db.relationship('Offer', backref='product')
+
+    
+
+
+
+
+class Offer(db.Model):
+    __tablename__= 'offer'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    offer_product_value = db.Column(db.Integer, nullable=False)
+    photo = db.Column(db.String(200), nullable=False)
+    product_description = db.Column(db.String(200), nullable=False)
+    state = db.Column(db.Boolean, nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'))
+    offers= db.relationship('Publication', backref='offer')
